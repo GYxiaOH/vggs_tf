531b6da40ab17fd42b06c13ea3e76e1d77f867bf
GYxiaOH <zhhy1994226@163.com>
2018-04-14 17:31:00 +0800

    first basic function

---
+++ finetune.py
@@ -0,0 +1,221 @@
+"""Script to finetune vggs using Tensorflow.
+
+Thank you for Frederik Kratzert write finetune Alexnex using tensorflow
+Because my experience use a seldom framework vggs ,so i fix his code
+i can use it in tensorflow with vision 1.2 or 1.4 and comes with a blog
+post, which you can find here:
+
+
+
+Author: GYxiaOH
+contact: f.kratzert(at)gmail.com
+"""
+
+import os
+
+import numpy as np
+import tensorflow as tf
+
+#from alexnet import AlexNt
+from vggsnet import VggsNet
+from datagenerator import ImageDataGenerator
+from datetime import datetime
+from tensorflow.contrib.data import Iterator
+
+"""
+Configuration Part.
+"""
+
+# Path to the textfiles for the trainings and validation set
+train_file = './sourcetrainlmdb.txt'
+val_file = './sourceval.txt'
+
+# Learning paramsi
+global_step = tf.Variable(0, trainable=False)
+learning_rate = tf.train.exponential_decay(0.01, global_step, 5000, 0.9, staircase=True)
+# learning_rate = 0.01 #0.01
+num_epochs = 100
+batch_size = 128
+
+#display and validation
+display_step = 20
+validation_epoch = 3
+
+# Network params
+dropout_rate = 0.5
+num_classes = 129
+train_layers = ['fc8'] # fc8 7 6
+
+# How often we want to write the tf.summary data to disk
+display_step = 10
+
+# Path for tf.summary.FileWriter and to store model checkpoints
+filewriter_path = "./tensorboard"
+checkpoint_path = "./checkpoints"
+
+"""
+Main Part of the finetuning Script.
+"""
+
+# Create parent path if it doesn't exist
+if not os.path.isdir(checkpoint_path):
+    os.mkdir(checkpoint_path)
+
+# Place data loading and preprocessing on the cpu
+with tf.device('/cpu:0'):
+    tr_data = ImageDataGenerator(train_file,
+                                 mode='training',
+                                 batch_size=batch_size,
+                                 num_classes=num_classes,
+                                 shuffle=True)
+    val_data = ImageDataGenerator(val_file,
+                                  mode='inference',
+                                  batch_size=batch_size,
+                                  num_classes=num_classes,
+                                  shuffle=False)
+
+    # create an reinitializable iterator given the dataset structure
+    iterator = Iterator.from_structure(tr_data.data.output_types,
+                                       tr_data.data.output_shapes)
+    next_batch = iterator.get_next()
+
+# Ops for initializing the two different iterators
+training_init_op = iterator.make_initializer(tr_data.data)
+validation_init_op = iterator.make_initializer(val_data.data)
+
+# TF placeholder for graph input and output
+x = tf.placeholder(tf.float32, [batch_size, 224, 224, 3])
+y = tf.placeholder(tf.float32, [batch_size, num_classes])
+keep_prob = tf.placeholder(tf.float32)
+
+# Initialize model
+model = VggsNet(x, keep_prob, num_classes, train_layers,'../DeViSE-master/prevgg.npy')
+
+# Link variable to model output
+score = model.fc8
+
+# List of trainable variables of the layers we want to train
+var_list = [v for v in tf.trainable_variables() if v.name.split('/')[0] in train_layers]
+
+# Op for calculating the loss
+with tf.name_scope("cross_net"):
+    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=score,
+                                                                  labels=y))
+
+# Train op
+with tf.name_scope("train"):
+    # Get gradients of all trainable variables
+    gradients = tf.gradients(loss, var_list)
+    gradients = list(zip(gradients, var_list))
+
+    # Create optimizer and apply gradient descent to the trainable variables
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+    train_op = optimizer.apply_gradients(grads_and_vars=gradients)
+
+# Add gradients to summary
+for gradient, var in gradients:
+    tf.summary.histogram(var.name + '/gradient', gradient)
+
+# Add the variables we train to the summary
+for var in var_list:
+    tf.summary.histogram(var.name, var)
+
+# Add the loss to summary
+tf.summary.scalar('cross_entropy', loss)
+
+
+# Evaluation op: Accuracy of the model
+with tf.name_scope("accuracy"):
+    correct_pred = tf.equal(tf.argmax(score, 1), tf.argmax(y, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+# Add the accuracy to the summary
+tf.summary.scalar('accuracy', accuracy)
+
+# Merge all summaries together
+merged_summary = tf.summary.merge_all()
+
+# Initialize the FileWriter
+writer = tf.summary.FileWriter(filewriter_path)
+
+# Initialize an saver for store model checkpoints
+saver = tf.train.Saver()
+
+# Get the number of training/validation steps per epoch
+train_batches_per_epoch = int(np.floor(tr_data.data_size/batch_size))
+val_batches_per_epoch = int(np.floor(val_data.data_size / batch_size))
+
+# Start Tensorflow session
+with tf.Session() as sess:
+
+    # Initialize all variables
+    sess.run(tf.global_variables_initializer())
+
+    # Add the model graph to TensorBoard
+    writer.add_graph(sess.graph)
+
+    # Load the pretrained weights into the non-trainable layer
+    model.load_initial_weights(sess)
+
+    print("{} Start training...".format(datetime.now()))
+    print("{} Open Tensorboard at --logdir {}".format(datetime.now(),
+                                                      filewriter_path))
+
+    # Loop over number of epochs
+    for epoch in range(num_epochs):
+
+        print("{} Epoch number: {}".format(datetime.now(), epoch+1))
+
+        # Initialize iterator with the training dataset
+        sess.run(training_init_op)
+
+        for step in range(train_batches_per_epoch):
+
+            # get next batch of data
+            img_batch, label_batch = sess.run(next_batch)
+
+            # And run the training op
+            _,l = sess.run([train_op,loss], feed_dict={x: img_batch,
+                                          y: label_batch,
+                                          keep_prob: dropout_rate})
+
+            # Generate summary with the current batch of data and print current los
+            if (step+1) % display_step == 0 :
+                s = sess.run(merged_summary, feed_dict={x: img_batch,
+                                                        y: label_batch,
+                                                        keep_prob: 1.})
+
+                writer.add_summary(s, epoch * train_batches_per_epoch + step)
+
+                print("Epoch " + str(epoch + 1) + " , Step " + str(step + 1) + ", Minibatch Loss= " + \
+                      "{:.4f}".format(l))
+        if (epoch + 1) % validation_epoch == 0:
+        # Validate the model on the entire validation set
+            print("{} Start validation".format(datetime.now()))
+            sess.run(validation_init_op)
+            val_acc = 0.
+            val_count = 0
+            val_loss = 0.
+            for _ in range(val_batches_per_epoch):
+
+                img_batch, label_batch = sess.run(next_batch)
+                vl,acc = sess.run([loss,accuracy], feed_dict={x: img_batch,
+                                                    y: label_batch,
+                                                    keep_prob: 1.})
+                val_acc += acc
+                val_count += 1
+                val_loss += vl
+            val_acc /= val_count
+            val_loss /= val_count
+            print("Validation, Epoch " + str(epoch + 1) + ", Loss " + "{:.4f}".format(val_loss) + ", target Accuracy= " + \
+                  "{:.4f}".format(val_acc))
+
+            print("{} Saving checkpoint of model...".format(datetime.now()))
+
+            # save checkpoint of the model
+            checkpoint_name = os.path.join(checkpoint_path,
+                                           'model_epoch'+str(epoch+1)+'.ckpt')
+            save_path = saver.save(sess, checkpoint_name)
+
+            print("{} Model checkpoint saved at {}".format(datetime.now(),
+                                                           checkpoint_name))
